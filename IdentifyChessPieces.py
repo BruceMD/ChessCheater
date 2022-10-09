f491f3761d63aff64f8da6e556ab6c95b809bf71
BruceMD <bruce.dickie@jembi.org>
2022-10-09 19:27:08 +0200

    Start with chess piece images

---
+++ IdentifyChessPieces.py
@@ -0,0 +1,11 @@
+def buildModel():
+
+    pass
+
+
+def trainingData():
+    pass
+# what's probably going to happen is that the training data will mostly be the same as the testing data
+# the types of pieces are very finite as is
+# get collection of all images, then scale them to all be the same size 80X80 pxels
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import CaptureScreen
+import ReadImage
 
 
 def main():
-
-    CaptureScreen.capture()
-
+    # CaptureScreen.capture()
+    # ReadImage.buildModel()
+    ReadImage.loadModel()
 
 if __name__ == '__main__':
     main()
