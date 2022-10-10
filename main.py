fc9af1c7dbe413ead3dff74912c08d10d472c90a
BruceMD <bruce.dickie@jembi.org>
2022-10-10 21:05:51 +0200

    Checkpoint

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def main():
 
     pass
     # CaptureScreen.capture()
-    # ReadImage.buildModel()                    # build machine model and save
+    # ReadImageDemo.buildModel()                    # build machine model and save
     # ReadImageDemo.loadModel()                 # load model and make predictions
-    # IdentifyChessPieces
-    BuildImageDataset.createDataset()         # create a numpy array of images
-    BuildImageDataset.loadDataset()             # load the numpy array, probably remove in future
+
+    BuildImageDataset.createTrainingDataset()         # create a numpy array of images
+    BuildImageDataset.createTestingDataset()         # create a numpy array of images
+    # # BuildImageDataset.loadDataset()             # load the numpy array, probably remove in future
+    #
+    IdentifyChessPieces.buildModel()
 
 
 if __name__ == '__main__':
