09d64befa3bcef4b401b9173022af1bf9e3fd5a7
BruceMD <bruce.dickie@jembi.org>
2022-10-10 16:40:08 +0200

    Update

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import CaptureScreen
-import ReadImage
+import IdentifyChessPieces
+import ReadImageDemo
+import BuildImageDataset
 
 
 def main():
+
+    pass
     # CaptureScreen.capture()
-    # ReadImage.buildModel()
-    ReadImage.loadModel()
+    # ReadImage.buildModel()                    # build machine model and save
+    # ReadImageDemo.loadModel()                 # load model and make predictions
+    # IdentifyChessPieces
+    BuildImageDataset.createDataset()         # create a numpy array of images
+    BuildImageDataset.loadDataset()             # load the numpy array, probably remove in future
+
 
 if __name__ == '__main__':
     main()
