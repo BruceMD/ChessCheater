fc24a4dcb03d27f9f559502e55a56bc17a3d1ebe
BruceMD <bruce.dickie@jembi.org>
2022-10-12 22:45:10 +0200

    Removed old method, tidying up

--- CaptureScreen.py
+++ CaptureScreen.py
@@ -69,30 +69,7 @@ def findCorner(y, x, screenImage, cellColour):
     return True
 
 
-def saveCornersNumpy():
-    screenImage = pag.screenshot()
-
-    topLeft = screenImage.crop(box=(2220, 165, 2230, 175))
-    topRight = screenImage.crop(box=(3028, 165, 3038, 175))
-    bottomLeft = screenImage.crop(box=(2220, 975, 2230, 985))
-    bottomRight = screenImage.crop(box=(3028, 975, 3038, 985))
-
-    np.save("Corners/topLeft", np.array(topLeft))
-    np.save("Corners/topRight", np.array(topRight))
-    np.save("Corners/bottomLeft", np.array(bottomLeft))
-    np.save("Corners/bottomRight", np.array(bottomRight))
-
-    plt.subplot(2, 2, 1)
-    plt.imshow(topLeft)
-    plt.subplot(2, 2, 2)
-    plt.imshow(topRight)
-    plt.subplot(2, 2, 3)
-    plt.imshow(bottomLeft)
-    plt.subplot(2, 2, 4)
-    plt.imshow(bottomRight)
-    plt.show()
-
-
+# a bit irrelevant at the moment
 def locate():
     sleep(3)
     for i in range(10):
